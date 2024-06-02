b89c727140939e14b6d837c19d6fb8f954f69d60
Manish Kumar <84091739+manish079@users.noreply.github.com>
2024-06-02 12:11:12 +0530

    setup

---
+++ 06_Exploring_The_World/src/components/Body.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+
+/* const Body = () => {
+  return (
+    <div className="body-container">
+      <div className="search">Search</div>
+      <div className="restaurant-container">
+        
+        <RestaurantCard
+          name="Shinde Dairy"
+          image="https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGl6emF8ZW58MHx8MHx8fDA%3D"
+        />
+        <RestaurantCard
+          name="Ghasitaram"
+          image="https://images.unsplash.com/photo-1590947132387-155cc02f3212?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHBpenphfGVufDB8fDB8fHww"
+        />
+        <RestaurantCard
+          name="Irani Cafe"
+          image="https://images.unsplash.com/photo-1588315029754-2dd089d39a1a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHBpenphfGVufDB8fDB8fHww"
+        />
+      </div>
+    </div>
+  );
+}; */
+
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Body;
---
+++ 06_Exploring_The_World/src/utils/constants.js
@@ -0,0 +1,3 @@
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/z/";
+export const LOGO_URL =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsRDkQoLVDDilFi-J1TbbGr5Xf3YCRP3UjKg&s";
