2a3cabfc4614c31f9be48ba255c67d8b11f9cb4b
Manish Kumar <84091739+manish079@users.noreply.github.com>
2024-06-03 00:41:16 +0530

    logo changes

--- 06_Exploring_The_World/src/utils/constants.js
+++ 06_Exploring_The_World/src/utils/constants.js
@@ -1,7 +1,7 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/z/";
 
 export const LOGO_URL =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsRDkQoLVDDilFi-J1TbbGr5Xf3YCRP3UjKg&s";
+  "https://png.pngtree.com/png-vector/20220708/ourmid/pngtree-fast-food-logo-png-image_5763171.png";
 
 export const SWIGGY_API =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING";
