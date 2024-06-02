3c1b1cff63756816b0311033beb5bca7062847dd
Manish Kumar <84091739+manish079@users.noreply.github.com>
2024-06-03 00:12:48 +0530

    search restaurants features

--- 06_Exploring_The_World/src/components/Body.js
+++ 06_Exploring_The_World/src/components/Body.js
@@ -5,11 +5,14 @@ import { SWIGGY_API } from "../utils/constants";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [filteredRestaurants, setFilterRestaurants] = useState(resList);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  console.log("Body Rendered");
   const fetchData = async () => {
     const response = await fetch(SWIGGY_API);
 
@@ -20,25 +23,52 @@ const Body = () => {
 
     //API not correctly working So I am fetching res data from mockData SO here I am not set data
     // setListOfRestaurants( jsonData?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle?.info);
+    // setFilteredRestaurants( jsonData?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle?.info);
   };
 
   return (
     <div className="body">
       <div className="filter">
+        <div className="search-bar">
+          <input
+            type="text"
+            className="search-text"
+            placeholder="Search"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              const filterRest = listOfRestaurants.filter((res) => {
+                return res.data.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase());
+              });
+              setFilterRestaurants(filterRest);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
-            const filteredList = listOfRestaurants.filter(
+            //If iterate over filterRestaurants then it give filter on visible cards but if I am do iterate over listOfRestaurants then it give filter on whole list of cards.
+
+            const filteredList = filteredRestaurants.filter(
               (res) => res.data.avgRating > 4
             );
-            setListOfRestaurants(filteredList);
+            setFilterRestaurants(filteredList);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant?.data?.id} resData={restaurant} />
         ))}
       </div>
