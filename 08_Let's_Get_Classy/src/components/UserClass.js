86ef381d262599f4c2d510a797aa0985937abd90
Manish Kumar <84091739+manish079@users.noreply.github.com>
2024-06-09 22:24:28 +0530

    State Variable and Update State

--- 08_Let's_Get_Classy/src/components/UserClass.js
+++ 08_Let's_Get_Classy/src/components/UserClass.js
@@ -3,11 +3,37 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count1: 0,
+      count2: 1,
+      count3: 0,
+    };
   }
   render() {
     const { name, location } = this.props; // destructuring
+    const { count1, count2 } = this.state;
+
+    console.log("Class component rendered");
+
     return (
       <div className="user-card">
+        <h1>count1: {count1}</h1>
+        <button
+          onClick={() => {
+            //Never update State variable directly
+            //this.state.count1 = this.state.count1 + 1;
+
+            this.setState({
+              count1: this.state.count1 + 1,
+              //React reconciliation algo only update value of count 1 it will not touch to count2 and count3
+            });
+
+            //On every increase value react do re-render class component same like in function component
+            //It only changes the component where we have made changes
+          }}
+        >
+          Count Increase
+        </button>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: @manish79</h4>
