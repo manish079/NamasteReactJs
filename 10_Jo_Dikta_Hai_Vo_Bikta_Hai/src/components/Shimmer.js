ed0de14d0b608a08e141828ff16a994de653e729
Manish Kumar <84091739+manish079@users.noreply.github.com>
2024-06-17 00:46:57 +0530

    tailwind_css_design

---
+++ 10_Jo_Dikta_Hai_Vo_Bikta_Hai/src/components/Shimmer.js
@@ -0,0 +1,106 @@
+const Shimmer = () => {
+  return (
+    <>
+      <div className="shimmer-container">
+        <div className="shimmer-card">
+          <div className="shimmer-content">
+            <h3 className="shimmer-name"></h3>
+            <h4 className="shimmer-type"></h4>
+            <h4 className="shimmer-rating"></h4>
+            <h4 className="shimmer-time"></h4>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-content">
+            <h3 className="shimmer-name"></h3>
+            <h4 className="shimmer-type"></h4>
+            <h4 className="shimmer-rating"></h4>
+            <h4 className="shimmer-time"></h4>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-content">
+            <h3 className="shimmer-name"></h3>
+            <h4 className="shimmer-type"></h4>
+            <h4 className="shimmer-rating"></h4>
+            <h4 className="shimmer-time"></h4>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-content">
+            <h3 className="shimmer-name"></h3>
+            <h4 className="shimmer-type"></h4>
+            <h4 className="shimmer-rating"></h4>
+            <h4 className="shimmer-time"></h4>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-content">
+            <h3 className="shimmer-name"></h3>
+            <h4 className="shimmer-type"></h4>
+            <h4 className="shimmer-rating"></h4>
+            <h4 className="shimmer-time"></h4>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-content">
+            <h3 className="shimmer-name"></h3>
+            <h4 className="shimmer-type"></h4>
+            <h4 className="shimmer-rating"></h4>
+            <h4 className="shimmer-time"></h4>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-content">
+            <h3 className="shimmer-name"></h3>
+            <h4 className="shimmer-type"></h4>
+            <h4 className="shimmer-rating"></h4>
+            <h4 className="shimmer-time"></h4>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-content">
+            <h3 className="shimmer-name"></h3>
+            <h4 className="shimmer-type"></h4>
+            <h4 className="shimmer-rating"></h4>
+            <h4 className="shimmer-time"></h4>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-content">
+            <h3 className="shimmer-name"></h3>
+            <h4 className="shimmer-type"></h4>
+            <h4 className="shimmer-rating"></h4>
+            <h4 className="shimmer-time"></h4>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-content">
+            <h3 className="shimmer-name"></h3>
+            <h4 className="shimmer-type"></h4>
+            <h4 className="shimmer-rating"></h4>
+            <h4 className="shimmer-time"></h4>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-content">
+            <h3 className="shimmer-name"></h3>
+            <h4 className="shimmer-type"></h4>
+            <h4 className="shimmer-rating"></h4>
+            <h4 className="shimmer-time"></h4>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-content">
+            <h3 className="shimmer-name"></h3>
+            <h4 className="shimmer-type"></h4>
+            <h4 className="shimmer-rating"></h4>
+            <h4 className="shimmer-time"></h4>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Shimmer;
