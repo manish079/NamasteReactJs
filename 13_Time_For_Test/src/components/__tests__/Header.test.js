002cf2bfbd0eeecdf9ff6948a97cd916b8662802
Manish Kumar <84091739+manish079@users.noreply.github.com>
2024-08-20 20:13:47 +0530

    describe and it

--- 13_Time_For_Test/src/components/__tests__/Header.test.js
+++ 13_Time_For_Test/src/components/__tests__/Header.test.js
@@ -22,7 +22,8 @@ test("Should button into contact component", () => {
   expect(button).toBeInTheDocument();
 });
 
-test("Should load 2 input boxes on the contact component", () => {
+//We can Write "it" also in place of test It just for naming convenience
+it("Should load 2 input boxes on the contact component", () => {
   render(<Contact />);
 
   //Querying
@@ -31,4 +32,27 @@ test("Should load 2 input boxes on the contact component", () => {
 
   //Assertion
   expect(inputBoxes / length).toBe(2); //not.toBe(2);
+});
+
+//Grouping test cases
+describe("Contact Us Page Test Case", () => {
+  test("Should button into contact component", () => {
+    render(<Contact />);
+
+    const button = screen.getByRole("button");
+
+    //Assertion
+    expect(button).toBeInTheDocument();
+  });
+
+  test("Should load 2 input boxes on the contact component", () => {
+    render(<Contact />);
+
+    //Querying
+    const inputBoxes = screen.getAllByRole("input"); //This will return JSX element(react fiber note, virtual dom, react element, object all are same thing)
+    //   console.log(inputBoxes);
+
+    //Assertion
+    expect(inputBoxes / length).toBe(2); //not.toBe(2);
+  });
 });
